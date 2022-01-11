5e28a45b2435122b850ff15a835e17b2bd251b12
ducnv1911@gmail.com <ducnv1911@gmail.com>
2022-01-11 10:55:08 +0700

    API add multisig transaction

---
+++ src/entities/index.ts
@@ -0,0 +1,6 @@
+export * from './aura-tx.entity';
+export * from './chain.entity';
+export * from './multisig-confirm.entity';
+export * from './multisig-transaction.entity';
+export * from './safe-owner.entity';
+export * from './safe.entity';
