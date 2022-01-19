fd9117af200b947bfe379edbd52e2d2e084ebb5d
Phan Anh Tuan <fibonacci998@gmail.com>
2022-01-19 10:45:46 +0700

    add swagger path

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { ConfigService } from './shared/services/config.service';
+import { SharedModule } from './shared/shared.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -8,10 +10,14 @@ async function bootstrap() {
   // enable cors
   app.enableCors();
   
+  const configService = app.select(SharedModule).get(ConfigService);
+  
   //setup swagger
   const config = new DocumentBuilder()
     .setTitle('Multisig Wallet API for Aura Network')
     .setVersion('0.1')
+    .addServer('/')
+    .addServer(configService.get('SWAGGER_PATH'))
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
