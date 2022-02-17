100e60daccc07766344d022646087edbc1c03e6b
Noah <doquockhanhan@gmail.com>
2022-02-17 13:27:29 +0700

    update repository to query txs from both tables

--- src/repositories/impls/transaction.repository.ts
+++ src/repositories/impls/transaction.repository.ts
@@ -24,23 +24,34 @@ export class TransactionRepository
     async getAuraTx(safeAddress: string, pageIndex: number, pageSize: number) {
         const limit = pageSize;
         const offset = limit * (pageIndex - 1);
-        let sqlQuerry = this.repos
-            .createQueryBuilder('auraTx')
-            .where('auraTx.fromAddress = :safeAddress', { safeAddress })
-            .orWhere('auraTx.toAddress = :safeAddress', { safeAddress })
-            .select([
-                'auraTx.id as id',
-                'auraTx.createdAt as createdAt',
-                'auraTx.updatedAt as updatedAt',
-                'auraTx.fromAddress as fromAddress',
-                'auraTx.toAddress as toAddress',
-                'auraTx.txHash as txHash',
-                'auraTx.amount as amount',
-                'auraTx.denom as denom',
-            ])
-            .limit(limit).offset(offset)
-            .orderBy('auraTx.createdAt', 'DESC');
-        let resultData = await sqlQuerry.getRawMany();
+        // let sqlQuerry = this.repos
+        //     .createQueryBuilder('auraTx')
+        //     .where('auraTx.fromAddress = :safeAddress', { safeAddress })
+        //     .orWhere('auraTx.toAddress = :safeAddress', { safeAddress })
+        //     .select([
+        //         'auraTx.id as id',
+        //         'auraTx.createdAt as createdAt',
+        //         'auraTx.updatedAt as updatedAt',
+        //         'auraTx.fromAddress as fromAddress',
+        //         'auraTx.toAddress as toAddress',
+        //         'auraTx.txHash as txHash',
+        //         'auraTx.amount as amount',
+        //         'auraTx.denom as denom',
+        //     ])
+        //     .limit(limit).offset(offset)
+        //     .orderBy('auraTx.createdAt', 'DESC');
+        // let resultData = await sqlQuerry.getRawMany();
+        let resultData = await this.repos
+            .query(`
+                SELECT Id, CreatedAt, UpdatedAt, FromAddress, ToAddress, TxHash, Amount, Denom, Status
+                FROM MultisigTransaction
+                WHERE FromAddress = ? OR ToAddress = ?
+                UNION
+                SELECT Id, CreatedAt, UpdatedAt, FromAddress, ToAddress, TxHash, Amount, Denom, Code
+                FROM AuraTx
+                WHERE FromAddress = ? OR ToAddress = ?
+                LIMIT ? OFFSET ?;
+            `, [ safeAddress, safeAddress, safeAddress, safeAddress, limit, offset ]);
         return resultData;
     }
 }
