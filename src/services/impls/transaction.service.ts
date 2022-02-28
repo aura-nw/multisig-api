cbfabd9fd64e4132076557f26854a906f79f2467
ducnv1911@gmail.com <ducnv1911@gmail.com>
2022-02-28 11:15:50 +0700

    Update transaction failed when broadcast failed

--- src/services/impls/transaction.service.ts
+++ src/services/impls/transaction.service.ts
@@ -220,6 +220,8 @@ export class TransactionService
           await this.multisigTransactionRepos.update(multisigTransaction);
         }
         else{
+          multisigTransaction.status = TRANSACTION_STATUS.FAILED;
+          await this.multisigTransactionRepos.update(multisigTransaction);
           this._logger.error(`${ErrorMap.E500.Code}: ${ErrorMap.E500.Message}`);
           this._logger.error(`${error.name}: ${error.message}`);
           this._logger.error(`${error.stack}`);
