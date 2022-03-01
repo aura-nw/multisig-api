ee53f6d71ee7fa24f9c431bb591156334aeb5401
ducnv1911@gmail.com <ducnv1911@gmail.com>
2022-03-01 10:57:29 +0700

    Add example

--- src/dtos/requests/transaction/send-transaction.request.ts
+++ src/dtos/requests/transaction/send-transaction.request.ts
@@ -2,17 +2,20 @@ import { ApiProperty } from '@nestjs/swagger';
 
 export class SendTransactionRequest {
     @ApiProperty({
-        description: 'Offchain Transaction Id'
+        description: 'Offchain Transaction Id',
+        example: 14
     })
     transactionId: number;
 
     @ApiProperty({
-        description: 'Chain Id'
+        description: 'Offchain Chain Id',
+        example: 4
     })
     internalChainId: number;
 
     @ApiProperty({
-        description: 'Owner who broadcast transaction'
+        description: 'Owner who broadcast transaction',
+        example: 'aura1l5k37zpxp3ukty282kn6r7kf8rqh7cve0ggq2w'
     })
     owner: string;
 }
