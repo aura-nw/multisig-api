6a4f425ec76f27b05be34fad5200082669f3cf12
harisato <pp.quochai@gmail.com>
2022-03-10 17:00:21 +0700

    Update safe decorator

--- src/decorators/common.decorator.ts
+++ src/decorators/common.decorator.ts
@@ -6,29 +6,48 @@ import {
   HttpStatus,
   Post,
 } from '@nestjs/common';
-import { ApiBadRequestResponse, ApiOperation } from '@nestjs/swagger';
+import {
+  ApiBadRequestResponse,
+  ApiOkResponse,
+  ApiOperation,
+  ApiResponseOptions,
+} from '@nestjs/swagger';
 
 interface Options {
   url?: string;
   summary: string;
+  apiOkResponseOptions?: ApiResponseOptions;
 }
 
 export function CommonPost(options: Options) {
-  return applyDecorators(Common(options.summary), Post(options.url));
+  return applyDecorators(
+    Common(options.summary, options.apiOkResponseOptions),
+    Post(options.url),
+  );
 }
 
 export function CommonGet(options: Options) {
-  return applyDecorators(Common(options.summary), Get(options.url));
+  return applyDecorators(
+    Common(options.summary, options.apiOkResponseOptions),
+    Get(options.url),
+  );
 }
 
 export function CommonDelete(options: Options) {
-  return applyDecorators(Common(options.summary), Delete(options.url));
+  return applyDecorators(
+    Common(options.summary, options.apiOkResponseOptions),
+    Delete(options.url),
+  );
 }
 
-export function Common(summary: string) {
+export function Common(
+  summary: string,
+  apiOkResponseOptions?: ApiResponseOptions,
+) {
   return applyDecorators(
     ApiBadRequestResponse({ description: 'Error: Bad Request', schema: {} }),
     HttpCode(HttpStatus.OK),
     ApiOperation({ summary }),
+    ApiOkResponse(apiOkResponseOptions),
   );
 }
