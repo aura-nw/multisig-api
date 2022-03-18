81ff7ea27e8c6e0db3bf5faa983556b070d76846
ducnv1911@gmail.com <ducnv1911@gmail.com>
2022-03-18 16:11:07 +0700

    Validate input param

--- src/dtos/requests/multisig-wallet/create-multisig-wallet.request.ts
+++ src/dtos/requests/multisig-wallet/create-multisig-wallet.request.ts
@@ -1,18 +1,24 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsNumber, IsString } from 'class-validator';
 
 export class CreateMultisigWalletRequest {
+  @IsString()
   @ApiProperty()
   creatorAddress: string;
 
+  @IsString()
   @ApiProperty()
   creatorPubkey: string;
 
+  @IsString()
   @ApiProperty({ type: [String] })
   otherOwnersAddress: string[];
 
+  @IsNumber()
   @ApiProperty()
   threshold: number;
 
+  @IsNumber()
   @ApiProperty()
   internalChainId: number;
 }
