03dc92460bc89f6dff5d22e93409e7c49acf0fa6
AnDQK <AnDQK@fsoft.com.vn>
2022-04-19 13:41:26 +0700

    complete create, confirm, reject, send execute contract message

--- src/repositories/ismart-contract.repository.ts
+++ src/repositories/ismart-contract.repository.ts
@@ -5,4 +5,46 @@ export interface ISmartContractRepository extends IBaseRepository {
    * Validate safe don't have pending tx
    */
   validateCreateTx(from: string): Promise<any>;
+
+  /**
+   * Insert data into smart contract tx table
+   */
+  insertExecuteContract(
+    from: string,
+    contract: string,
+    functionName: string,
+    parameters: string,
+    gas: number,
+    fee: number,
+    status: string,
+    typeUrl: string,
+    internalChainId: number,
+    denom: string,
+    accountNumber: number,
+    sequence: string,
+    safeId: number
+  ): Promise<any>;
+
+  /**
+   * Check exist smart contract tx
+   */
+  checkExistSmartContractTx(
+    smartContractTxId: number,
+    internalChainId: number
+  ): Promise<any>;
+
+  /**
+   * Validate transaction
+   */
+   validateTransaction(transactionId: number, internalChainId: number): any;
+
+   /**
+   * Validate when send tx
+   */
+  validateTxBroadcast(transactionId: number): Promise<any>;
+
+  /**
+   * Update tx when broadcasted success
+   */
+   updateTxBroadcastSucces(transactionId: number, txHash: string): Promise<any>;
 }
