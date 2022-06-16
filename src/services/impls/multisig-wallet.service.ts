63f2fffe727561de3e7d5ac4568e91c91865b034
harisato <pp.quochai@gmail.com>
2022-06-16 15:45:56 +0700

    Update get balance evmos

--- src/services/impls/multisig-wallet.service.ts
+++ src/services/impls/multisig-wallet.service.ts
@@ -136,7 +136,7 @@ export class MultisigWalletService
             chainInfo.denom,
           );
           if (balance.denom === 'atevmos') {
-            const amount = Number(balance.amount) * 10 ** -9;
+            const amount = Number(balance.amount) * 10 ** -6;
             balance.amount = String(amount);
           }
           safeInfo.balance = [balance];
