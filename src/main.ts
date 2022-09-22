f006cacc03f0cfca1f63ebc8e9454516c07a00ad
Minh Bui <72347448+clanV2@users.noreply.github.com>
2022-09-22 10:06:46 +0700

    uncomment main

--- src/main.ts
+++ src/main.ts
@@ -15,19 +15,19 @@ async function bootstrap() {
 
   const configService = app.select(SharedModule).get(ConfigService);
 
-  //create or update chain info
-  // const seederService = app.select(SeederModule).get(SeederService);
-  // seederService
-  //   .seed()
-  //   .then(() => {
-  //     Logger.debug('Seed completed');
-  //   })
-  //   .catch((error) => {
-  //     Logger.error(error);
-  //     app.close().then(() => {
-  //       process.exit(1);
-  //     });
-  //   });
+  // create or update chain info
+  const seederService = app.select(SeederModule).get(SeederService);
+  seederService
+    .seed()
+    .then(() => {
+      Logger.debug('Seed completed');
+    })
+    .catch((error) => {
+      Logger.error(error);
+      app.close().then(() => {
+        process.exit(1);
+      });
+    });
 
   //setup swagger
   const config = new DocumentBuilder()
