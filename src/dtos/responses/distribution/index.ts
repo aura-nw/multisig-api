feeb9e86469987435879d90c99b827dafbb39749
harisato <pp.quochai@gmail.com>
2022-10-28 11:22:04 +0700

    Update distribution API

--- src/dtos/responses/distribution/index.ts
+++ src/dtos/responses/distribution/index.ts
@@ -1,4 +1,6 @@
+export * from './get-delegation.response';
 export * from './get-delegations.response';
 export * from './get-undelegations.response';
 export * from './get-validators.response';
-export * from './get-delegation.response';
+export * from './get-validator.request';
+export * from './get-validator.response';
