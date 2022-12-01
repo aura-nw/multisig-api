4fc7fe889ea5d199bbfa763128a6b8d1c291b056
harisato <pp.quochai@gmail.com>
2022-12-01 11:25:12 +0700

    Update example API

--- src/dtos/requests/transaction/simulate.request.ts
+++ src/dtos/requests/transaction/simulate.request.ts
@@ -7,7 +7,7 @@ export class SimulateTxRequest {
   @ApiProperty({
     description: 'Encoded Messages',
     example:
-      'W3sidHlwZVVybCI6Ii9jb3Ntb3Muc3Rha2luZy52MWJldGExLk1zZ0RlbGVnYXRlIiwidmFsdWUiOnsiZGVsZWdhdG9yQWRkcmVzcyI6ImF1cmExdjk1eHNlbjNkeXE1dXU0cjhxeDl5NW0yNmxrdWFhdDA3eGF6cGYiLCJ2YWxpZGF0b3JBZGRyZXNzIjoiYXVyYXZhbG9wZXIxZWR3NGx3Y3ozZXNubGd6Y3c2MHJhOG0zOGszenlnejJ4dGwycWgiLCJhbW91bnQiOnsiYW1vdW50IjoiMSIsImRlbm9tIjoidXRhdXJhIn19fV0=',
+      'W3sidHlwZVVybCI6Ii9jb3Ntb3Muc3Rha2luZy52MWJldGExLk1zZ0RlbGVnYXRlIiwidmFsdWUiOnsiZGVsZWdhdG9yQWRkcmVzcyI6ImF1cmExcDYwY2pzc2FjZXUwcThmOHQ5OWU4MDlnaDJueHJxOXJtcDVrYW0iLCJ2YWxpZGF0b3JBZGRyZXNzIjoiYXVyYXZhbG9wZXIxZWR3NGx3Y3ozZXNubGd6Y3c2MHJhOG0zOGszenlnejJ4dGwycWgiLCJhbW91bnQiOnsiYW1vdW50IjoiMSIsImRlbm9tIjoidXRhdXJhIn19fV0=',
   })
   encodedMsgs: string;
 
