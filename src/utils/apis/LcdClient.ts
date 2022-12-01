343a245ef8ba999133140e1b2aca427635bf580c
harisato <pp.quochai@gmail.com>
2022-12-01 11:25:21 +0700

    Handle error when simulate fail

--- src/utils/apis/LcdClient.ts
+++ src/utils/apis/LcdClient.ts
@@ -1,3 +1,5 @@
+import { CustomError } from 'src/common/customError';
+import { ErrorMap } from 'src/common/error.map';
 import { SimulateResponse } from 'src/dtos/responses/simulate';
 import { ConfigService } from 'src/shared/services/config.service';
 import { CommonUtil } from '../common.util';
@@ -12,12 +14,16 @@ export class LcdClient {
   }
 
   async simulate(txBytesBase64: string): Promise<SimulateResponse> {
-    const url = new URL(`cosmos/tx/v1beta1/simulate`, this.lcdUrl);
-    const simulateRes = await this.request(url.href, 'POST', {
-      tx_bytes: txBytesBase64,
-    });
-    return {
-      gasUsed: simulateRes.gas_info?.gas_used || 0,
-    };
+    try {
+      const url = new URL(`cosmos/tx/v1beta1/simulate`, this.lcdUrl);
+      const simulateRes = await this.request(url.href, 'POST', {
+        tx_bytes: txBytesBase64,
+      });
+      return {
+        gasUsed: simulateRes.gas_info?.gas_used || 0,
+      };
+    } catch (error) {
+      throw new CustomError(ErrorMap.TX_SIMULATION_FAILED, error.message);
+    }
   }
 }
