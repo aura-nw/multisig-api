b3a13b3d9f3852b5cb0800808bb821ef6062efcb
harisato <pp.quochai@gmail.com>
2023-03-10 11:09:30 +0700

    Optimize Dockerfile

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,15 +7,15 @@ module.exports = {
     'unicorn',
   ],
   extends: [
-    'airbnb-base',
-    'airbnb-typescript/base',
+    'plugin:import/typescript',
+    'plugin:prettier/recommended',
+    'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:@typescript-eslint/recommended-requiring-type-checking',
     'plugin:eslint-comments/recommended',
     'plugin:jest/recommended',
     'plugin:promise/recommended',
     'plugin:unicorn/recommended',
-    'prettier',
   ],
   env: {
     node: true,
