ca07feddcdd83df7a2f52d9ba4dae79a6ee422d4
harisato <pp.quochai@gmail.com>
2023-03-16 13:59:11 +0700

    Update notification job

--- src/modules/jobs/notify-proposal.jobs.ts
+++ src/modules/jobs/notify-proposal.jobs.ts
@@ -9,6 +9,7 @@ import { User } from '../user/entities/user.entity';
 import { IndexerClient } from '../../shared/services/indexer.service';
 import { NotificationRepository } from '../notification/notification.repository';
 import { IProposal } from '../../interfaces';
+import { Queue } from './queue';
 
 @Injectable()
 export class NotifyProposalJob {
@@ -27,9 +28,103 @@ export class NotifyProposalJob {
 
   @Cron('0 0 0 * * *')
   async handleCron() {
-    const currentTime = new Date();
+    const usersQueue = new Queue<User[]>();
+
     // Get all supported chains
     const supportedChains: Chain[] = await this.chainRepo.showNetworkList();
+
+    // Get proposals in voting period
+    const inVotingProposal = await this.getInVotingProposals(supportedChains);
+    if (inVotingProposal.length === 0) return;
+
+    // Get all users
+    let skip = 0;
+    const limit = 50;
+    const users: User[] = await this.userRepo.getUsers(limit, skip);
+    if (users.length === 0) return;
+
+    usersQueue.enqueue(users);
+
+    // Make notification templates
+    const templates = this.buildTemplateNotifications(
+      inVotingProposal,
+      supportedChains,
+    );
+
+    // Send notifications to users
+    while (!usersQueue.isEmpty()) {
+      const usersInQueue = usersQueue.dequeue();
+      if (usersInQueue) {
+        // eslint-disable-next-line no-await-in-loop
+        await this.sendNotificationToUser(usersInQueue, templates);
+
+        skip += limit;
+        // eslint-disable-next-line no-await-in-loop
+        const anotherUsers = await this.userRepo.getUsers(limit, skip);
+        if (anotherUsers.length > 0) {
+          usersQueue.enqueue(anotherUsers);
+        }
+      }
+    }
+  }
+
+  /**
+   *
+   * @param inVotingProposal
+   * @param supportedChains
+   * @returns
+   */
+  buildTemplateNotifications(
+    inVotingProposal: IProposal[],
+    supportedChains: Chain[],
+  ): Notification[] {
+    return inVotingProposal.map((proposal) =>
+      Notification.newProposalNotification(
+        Number(proposal.proposal_id),
+        proposal.content.title,
+        new Date(proposal.voting_end_time),
+        supportedChains.find(
+          (chain) => chain.chainId === proposal.custom_info.chain_id,
+        ).id,
+      ),
+    );
+  }
+
+  /**
+   *
+   * @param users
+   * @param templates
+   */
+  async sendNotificationToUser(users: User[], templates: Notification[]) {
+    // Notify to all users
+    const promises: Promise<Notification[]>[] = [];
+    templates.forEach((template) => {
+      const notifications = users.map((user) => {
+        const newNotification = { ...template };
+        newNotification.userId = user.id;
+        return plainToInstance(Notification, newNotification);
+      });
+
+      // Save notifications to DB
+      promises.push(this.notifyRepo.saveNotification(notifications));
+    });
+
+    const result = await Promise.all(promises);
+
+    for (const notifications of result) {
+      this.logger.log(
+        `[Chain #${notifications[0].internalChainId} - Proposal #${notifications[0].proposalNumber}] Send ${notifications.length} notifications to ${users.length} users successfully!`,
+      );
+    }
+  }
+
+  /**
+   *
+   * @param supportedChains
+   * @returns
+   */
+  async getInVotingProposals(supportedChains: Chain[]): Promise<IProposal[]> {
+    const currentTime = new Date();
 
     // Get latest proposal
     const latestProposals = await Promise.all(
@@ -49,43 +144,6 @@ export class NotifyProposalJob {
         ),
       );
     });
-    if (inVotingProposal.length === 0) return;
-
-    // Get all users
-    // Must get 1000 users/batch
-    const users: User[] = await this.userRepo.getAllUser();
-
-    // Make notification templates
-    const templates = inVotingProposal.map((proposal) =>
-      // Create template notification
-      Notification.newProposalNotification(
-        Number(proposal.proposal_id),
-        proposal.content.title,
-        new Date(proposal.voting_end_time),
-        supportedChains.find(
-          (chain) => chain.chainId === proposal.custom_info.chain_id,
-        ).id,
-      ),
-    );
-
-    // Must send a notification to the safe owner by a chain instead of reporting to all user
-    // Notify to all users
-    const promises = templates.map((template) => {
-      const notifications = users.map((user) => {
-        const newNotification = { ...template };
-        newNotification.userId = user.id;
-        return plainToInstance(Notification, newNotification);
-      });
-
-      // Save notifications to DB
-      return this.notifyRepo.saveNotification(notifications);
-    });
-
-    await Promise.all(promises);
-    this.logger.log(
-      `Notified proposal ${templates.map((t) => t.id).toString()} to ${
-        users.length
-      } users successfully!`,
-    );
+    return inVotingProposal;
   }
 }
